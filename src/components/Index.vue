42544211f053aa8134312c133292d557c9ba23d7
wangj <2630929821@qq.com>
2019-09-05 15:59:43 +0800

    1

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -13,7 +13,7 @@
             </div>
             <div class="inputIn">
                 <el-input :placeholder="placeholder" v-model="videoSrc" clearable></el-input>
-                <el-button type="primary" icon="el-icon-search">解析视频</el-button>
+                <el-button type="primary" :icon="icon">解析视频</el-button>
             </div>
             <div class="bottom">
                 <div class="bottomTitle">
@@ -47,12 +47,14 @@ export default {
             placeholder:'请复制APP分享链接到这里',//视频链接的提示文字
             videoSrc:'',//视频链接
             problemImg:'http://douyin.shulijp.com/img/iImg8.png',//问题图标
+            //问题列表
             problemInfo:[
                 {key:'1. Android手机上可以用吗？',value:'可以，Android手机在常用的Chrome、UC、360、QQ等浏览器上都可以很方便的使用本站。推荐使用Chrome浏览器获得最佳下载体验。'},
                 {key:'2. iOS设备（iPhone、iPad、iPod）上点击下载视频按钮后，跳转到视频页面，并没有直接下载，怎么办？？',value:'因Safari及微信内置浏览器均不支持下载文件，所以保存视频需要借助第三方App来完成下载，建议iOS用户在App Store下载免费的Documents 6，然后在Documents的内置浏览器中使用本站，可以完美下载视频，并且Documents支持将下载的视频移到手机相册。参考: ',src:'https://www.jianshu.com/p/8a11cb308212',srcText:'iPhone上利用Documents下载和管理视频教程'},
                 {key:'3. 我在电脑上用的是IE浏览器，点下载视频按钮后，出现跟上面那用iPad的哥们一样的情况，跳转到视频页面，如何下载到本地呢？',value:'电脑上少数浏览器不支持直接下载，但可以在下载视频按钮上点击右键，然后选择"目标另存为"或"链接存储为"来下载视频；或者到跳转后的视频页面，在视频画面上点击右键，然后选择"视频另存为"来下载视频。当然，我们更推荐在电脑上使用如谷歌Chrome浏览器、360浏览器极速模式、QQ浏览器极速模式等现代浏览器来获得最佳上网体验。'},
                 {key:'4. 怎么查看我的剩余次数？',value:'点击头像 → 我的资料 ，同时有以下方式可获取额外的解析金额奖励：新注册、邀请好友、每日赠送等途径，解析次数用完之后可通过充值获取额外解析次数'}
-            ]
+            ],
+            icon:'el-icon-loading',//搜索框的图标  默认是 el-icon-search 搜索 点击后变成 el-icon-loading 加载状态 
 
         }
     }
